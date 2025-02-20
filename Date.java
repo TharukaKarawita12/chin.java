8316571126b572c1eb6ba10bcd9125a102fd6c9b
TharukaKarawita12 <s4142142@student.rmit.edu.au>
2025-02-20 14:12:24 +1100

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,12 @@
+public class Maths {
+    public static void main(String[] args) {
+        str Day = "Wednesday" ; 
+        str month = "February";
+        int date = 19
+        int year = 2025
+        
+        System.out.println("American format: " + Day + month + date + year );
+        System.out.println("European format: " + Day + date + month + year);
+        System.out.println("Japanese format: " + year + month + date + day);}
+    }
+ 
